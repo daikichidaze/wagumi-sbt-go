e5c445255444abbc7f78e2a247f04a53221cd6b5
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-08-28 09:51:40 +0700

    added json tags to metadata

--- metadata.go
+++ metadata.go
@@ -1,34 +1,32 @@
 package main
 
 type Metadata struct {
-	Name         string
-	Description  string
-	Image        string
-	External_url string
-	Properties   MetadetaProperty
+	Name         string           `json:"name"`
+	Description  string           `json:"description"`
+	Image        string           `json:"image"`
+	External_url string           `json:"external_url"`
+	Properties   MetadetaProperty `json:"properties"`
 }
 
 type MetadetaProperty struct {
-	// Sns           map[string]string
-	// Tokens        []map[string]string
-	Contribusions []Contribution
+	// Sns           map[string]string   `json:"sns"`
+	// Tokens        []map[string]string `json:"tokens"`
+	Contribusions []Contribution `json:"contributions"`
 }
 
 type Contribution struct {
-	Name        string
-	Description string
-	Image       string
-	ExternalUrl string
-	Properties  ContributionProperty
+	Name        string               `json:"name"`
+	Description string               `json:"description"`
+	Image       string               `json:"image"`
+	ExternalUrl string               `json:"external_url"`
+	Properties  ContributionProperty `json:"properties"`
 }
 
 type ContributionProperty struct {
-	PageId    string
-	Reference []string
+	PageId    string   `json:"page_id"`
+	Reference []string `json:"reference"`
 	Date      struct {
-		Start string
-		End   string
-	}
+		Start string `json:"start"`
+		End   string `json:"end"`
+	} `json:"date"`
 }
-
-
